f96ac314c4fd4e8e3f9242a55ded4bc2c343f922
kuldeep singh <kuldeeprathore74@gmail.com>
2020-12-24 20:39:48 +0530

    added more emoji and style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,90 @@ const emojiDictionary = {
   "😔": "sad",
   "🥡": "takeout box",
   "❤️": "love",
-  "😑": "annoyance"
+  "😑": "annoyance",
+  "🥰": "smiling face with hearts",
+  "😍": "smiling face with heart-eyes",
+  "🤩": "star-struck",
+  "😘": "face blowing a kiss",
+  "😗": "kissing face",
+  "☺️": "smiling face",
+  "☺": "smiling face",
+  "😚": "kissing face with closed eyes",
+  "😙": "kissing face with smiling eyes",
+  "😋": "face savoring food",
+  "😛": "face with tongue",
+  "😜": "winking face with tongue",
+  "🤪": "zany face",
+  "😝": "squinting face with tongue",
+  "🤗": "hugging face",
+  "🤭": "face with hand over mouth",
+  "🤫": "shushing face",
+  "🤔": "thinking face",
+  "🤨": "face with raised eyebrow",
+  "😐": "neutral face",
+
+  "😶": "face without mouth",
+  "😏": "smirking face",
+  "😒": "unamused face",
+  "🙄": "face with rolling eyes",
+  "😬": "grimacing face",
+  "🤥": "lying face",
+  "😌": "relieved face",
+
+  "😪": "sleepy face",
+  "🤤": "drooling face",
+  "😴": "sleeping face",
+  "😷": "face with medical mask",
+  "🤒": "face with thermometer",
+  "🤕": "face with head-bandage",
+  "🤢": "nauseated face",
+  "🤮": "face vomiting",
+  "🤧": "sneezing face",
+  "🥵": " hot face",
+  "🥶": " cold face",
+  "🥴": " woozy face",
+  "😵": "dizzy face",
+  "🤯": "exploding head",
+  "🤠": "cowboy hat face",
+  "🥳": " partying face",
+  "🥸": " disguised face",
+  "😎": "smiling face with sunglasses",
+  "🤓": "nerd face",
+  "🧐": "face with monocle",
+  "😕": "confused face",
+  "😟": "worried face",
+  "🙁": "slightly frowning face",
+  "☹️": "frowning face",
+  "☹": "frowning face",
+  "😮": "face with open mouth",
+  "😯": "hushed face",
+  "😲": "astonished face",
+
+  "🥺": " pleading face",
+  "😦": "frowning face with open mouth",
+  "😧": "anguished face",
+  "😨": "fearful face",
+  "😰": "anxious face with sweat",
+  "😥": "sad but relieved face",
+  "😢": "crying face",
+  "😭": "loudly crying face",
+  "😱": "face screaming in fear",
+  "😖": "confounded face",
+  "😣": "persevering face",
+  "😞": "disappointed face",
+  "😓": "downcast face with sweat",
+  "😩": "weary face",
+  "😫": "tired face",
+  "🥱": " yawning face",
+  "😤": "face with steam from nose",
+  "😡": "pouting face",
+  "😠": "angry face",
+  "🤬": "face with symbols on mouth",
+  "😈": "smiling face with horns",
+  "👿": "angry face with horns",
+  "💀": "skull",
+  "☠️": "skull and crossbones",
+  "☠": "skull and crossbones"
 };
 var emojisWeknow = Object.keys(emojiDictionary);
 export default function App() {
@@ -26,20 +109,21 @@ export default function App() {
   return (
     <div className="App">
       <h1>Emoji Lovers</h1>
-      <input onChange={emojiInputHandler} />
+      <input
+        onChange={emojiInputHandler}
+        placeholder="Enter Emoji to get meaning"
+      />
 
       <h2>{meaning}</h2>
       <h3>Emoji's we know</h3>
       {emojisWeknow.map(function (emoji) {
         return (
-          <span
-            onClick={() => emojiClickHandler(emoji)}
-            style={{ fontSize: "2rem", padding: "0.5", cursor: "pointer" }}
-            key={emoji}
-          >
+          // <div className="emoji-box">
+          <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
             {" "}
             {emoji}{" "}
           </span>
+          // </div>
         );
       })}
     </div>
